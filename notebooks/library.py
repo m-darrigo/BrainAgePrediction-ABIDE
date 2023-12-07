7a37fd58fe885e86ae06c1e3211a89bafa6e0148
Marco D'Arrigo <m.darrigo@studenti.unipi.it>
2023-12-07 23:42:59 +0100

    new function in lib

--- notebooks/library.py
+++ notebooks/library.py
@@ -39,8 +39,43 @@ def corr_distr(df_corr):
 
     return 1
 
+def top_corr_relations(df, y, target_variable="AGE_AT_SCAN", num_top_features=30):
+    '''
+    Identifies the top 30 features in a DataFrame based on their absolute correlation with a target variable.
+    
+    Creates a new DataFrame by combining the selected features and the target variable in a long format.
+    
+    Generates scatter plots with regression lines for each selected feature against a specified variable.
+    
+    The resulting visualizations offer insights into the relationship between the top correlated features and
+    the target variable across different subplots.
+    
+    The scatter plots illustrate how the values of these features vary concerning the specified variable.
+    '''
+    top_corr_features = (
+        np.abs(df.corrwith(y))
+        .sort_values(ascending=False)
+        .head(num_top_features)
+        .index
+    )
 
+    df_long = (
+        pd.concat((df[top_corr_features], y), axis=1)
+        .melt(id_vars=target_variable)
+    )
 
+    sns.lmplot(
+        data=df_long,
+        x="value",
+        y=target_variable,
+        col="variable",
+        col_wrap=3,
+        sharex=False,
+        sharey=False,
+        scatter_kws={"alpha": 0.3}
+    )
+
+    return 0
 
 
 
