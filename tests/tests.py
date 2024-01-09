68f8ad642ff954d6f46264678781628d239d28e3
Marco D'Arrigo <m.darrigo@studenti.unipi.it>
2024-01-09 18:27:07 +0100

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,26 @@
+""" Tests"""
 import unittest
-import os
-import sys
+import numpy as np
 
-#to run locally
-#package_name = "../BrainAgePrediction"
-package_name = "BrainAgePrediction"
+import library
 
-sys.path.insert(0, package_name)
 
-class TestBrainAge(unittest.TestCase):
-    """
-    Class for performing unit testing on code
-    """
-    def setUp(self):
-        """
-        Class setup.
-        """
-        self.data = package_name + "/dataset/FS_features_ABIDE_males.csv"
+class TestPreprocessing(unittest.TestCase):
+    def __init__(self):
+        self.df = pd.read_csv(...)
+
+    def test_alldistr_img(self):
+        # This is a test to see if the function runs without errors.
+        # The function is so simple that it does not need deep testing
+        library.all_distr_img(self.df)
+
+    # test if the function returns a ValueError when a wrong input is given.
+    def test_alldistr_img_wrong_dtype(self):
+        with self.assertRaises(ValueError):
+            library.all_distr_img(5)
+
+    # This type of testing can be utilized for all plotting functions
+
 
 if __name__ == "__main__":
     unittest.main()
