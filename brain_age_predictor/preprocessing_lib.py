cc9d08fbc7b37c2eab80ada50e4a21219d9d0367
Marco D'Arrigo <91227676+m-darrigo@users.noreply.github.com>
2024-01-21 17:55:47 +0100

    New docstrings

---
+++ brain_age_predictor/preprocessing_lib.py
@@ -0,0 +1,283 @@
+
+""" This module provides some function to read, explore and preprocess an input
+    dataframe cointaining a set of features.
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, PowerTransformer
+
+
+def alldistr_img(df: pd.DataFrame) -> None:
+    """
+    Plot the distributions of all features in a pandas DataFrame in a single image.
+
+    Parameters
+    -----------
+    df : pandas.DataFrame
+        The input dataset containing features for which distributions will be plotted.
+
+    Raises
+    -------
+    ValueError
+        If the input ``df`` is not a pandas DataFrame.
+
+    Note
+    ------
+    This function uses seaborn's displot to visualize the distributions of all features
+    in the input DataFrame. Each feature is represented in a separate subplot within
+    a single image. The resulting image is saved as "imgs/all_distributions.png" in the
+    current working directory.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from your_module import alldistr_img
+
+    >>> #Assuming 'df' is your dataset
+    >>> alldistr_img(df)
+    """
+
+    # dtype check
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df must be a pandas DataFrame")
+
+    sns.displot(
+        data=df.melt(),
+        x="value",
+        col="variable",
+        facet_kws={"sharey": False, "sharex": False},
+        common_bins=False,
+        col_wrap=5,
+    )
+
+    plt.savefig("imgs/all_distributions.png")
+    plt.close()
+
+    pass
+
+
+def corr_distr(df_corr: pd.DataFrame) -> None:
+    '''
+    Function that calculates the correlation and then generates a distribution.
+    '''
+    np.fill_diagonal(df_corr.values, 0)
+    bivariate_corr = df_corr.unstack().rename("correlation_coefficient").to_frame()
+    sns.histplot(bivariate_corr)
+    plt.xlabel("pearson correlation coefficient")
+
+    pass
+
+
+def top_corr_relations(
+        df: pd.DataFrame,
+        y: pd.Series,
+        target_variable: str = "AGE_AT_SCAN",
+        num_top_features: int = 30,
+) -> None:
+    """
+    Visualizes top correlated features with a target variable using scatter plots.
+
+    Calculates absolute correlation between each feature in df and the target variable (y).
+    Selects the top ``num_top_feature`` features based on correlation and generates
+    regression plots for each feature against the target variable.
+    Regression plots are arranged in a grid with 3 columns.
+
+    Parameters
+    ----------
+
+    df: pandas.DataFrame
+        Input DataFrame containing features.
+    y: pandas.Series
+        Target variable to be correlated with features.
+    target_variable: str
+        Column name of the target variable. Default is "AGE_AT_SCAN".
+    num_top_features: int
+        Number of top features to consider. Default is 30.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> top_corr_relations(df, y)
+    """
+    top_corr_features = (
+        np.abs(df.corrwith(y))
+        .sort_values(ascending=False)
+        .head(num_top_features)
+        .index
+    )
+
+    df_long = (
+        pd.concat((df[top_corr_features], y), axis=1)
+        .melt(id_vars=target_variable)
+    )
+
+    sns.lmplot(
+        data=df_long,
+        x="value",
+        y=target_variable,
+        col="variable",
+        col_wrap=3,
+        sharex=False,
+        sharey=False,
+        scatter_kws={"alpha": 0.3}
+    )
+
+    pass
+
+
+def scaled_distributions(df: pd.DataFrame) -> None:
+    """
+    Visualizes scaled distributions of columns with high variance.
+
+    This function takes a DataFrame and applies two scalers
+    (:py:obj:`StandardScaler <sklearn.preprocessing.StandardScaler>` and
+    :py:obj:`PowerTransformer <sklearn.preprocessing.PowerTransformer>`)
+    to columns with high variance. Subsequently, it visualizes the scaled distributions using
+    Seaborn's :py:func:`displot <seaborn.displot>`.
+
+    Parameters
+    -----------
+    df : pandas.DataFrame
+        The DataFrame containing the data to be analyzed.
+
+    Returns
+    --------
+    None
+
+    Raises
+    -------
+    ValueError
+        If ``df`` is not a pandas DataFrame.
+
+    Note
+    ------
+    This function identifies columns with high variance based on kurtosis, applies
+    :py:obj:`StandardScaler <sklearn.preprocessing.StandardScaler>` and
+    :py:obj:`PowerTransformer <sklearn.preprocessing.PowerTransformer>` to the selected columns,
+    and visualizes the scaled distributions using Seaborn's
+    :py:func:`displot <seaborn.displot>`.
+
+    The top 20 columns with the highest kurtosis values are chosen for visualization. Two scalers,
+    :py:obj:`StandardScaler <sklearn.preprocessing.StandardScaler>` and
+    :py:obj:`PowerTransformer <sklearn.preprocessing.PowerTransformer>`,
+    are applied to create two sets of scaled distributions.
+    Each set is displayed in a separate subplot with a title indicating the scaler used.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from library import scaled_distributions
+
+    >>> Assuming 'df' is your DataFrame
+    >>> scaled_distributions(df)
+    """
+    # dtype check
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df must be a pandas DataFrame")
+
+    # Identify columns with high variance based on kurtosis
+    high_variance_col = df.kurtosis().sort_values(ascending=False).head(20).index
+
+    # Define scalers
+    scalers = [StandardScaler(), PowerTransformer()]
+
+    # Define scaler names for plot titles
+    names = ["Standard Scaler", "PowerTransformer"]
+
+    # Iterate over scalers
+    for scaler, name in zip(scalers, names):
+        # Apply scaler and create a DataFrame with scaled values
+        scaled_df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+        # Visualize scaled distributions using Seaborn's displot
+        sns.displot(
+            data=scaled_df[high_variance_col].melt(),
+            x="value",
+            col="variable",
+            facet_kws={"sharey": False, "sharex": False},
+            common_bins=False,
+            col_wrap=5
+        )
+
+        plt.title(name)
+
+    pass
+
+
+def pca_variance(df: pd.DataFrame) -> None:
+    """
+    Visualizes the explained variance of Principal Components Analysis
+    (:py:obj:`PCA <sklearn.decomposition.PCA>`) on scaled data.
+
+    This function takes a DataFrame, applies two scalers
+    (:py:obj:`StandardScaler <sklearn.preprocessing.StandardScaler>` and
+    :py:obj:`PowerTransformer <sklearn.preprocessing.PowerTransformer>`),
+    performs PCA, and visualizes the explained variance.
+
+    Parameters
+    -----------
+    df : pandas.DataFrame
+        The DataFrame containing the data to be analyzed.
+
+    Returns
+    --------
+    None
+
+    Raises
+    -------
+    ValueError
+        If ``df`` is not a pandas DataFrame.
+
+    Note
+    ------
+    This function applies :py:obj:`StandardScaler <sklearn.preprocessing.StandardScaler>` and
+    :py:obj:`PowerTransformer <sklearn.preprocessing.PowerTransformer>` to the DataFrame,
+    performs PCA on the scaled data, and visualizes the explained variance.
+
+    The top 20 columns with the highest kurtosis values are chosen for PCA analysis.
+    The cumulative explained variance is plotted against the number of principal components.
+    A vertical dashed line indicates the top 20 components, providing insight into the
+    retained variance with a specific number of principal components.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from library import pca_variance
+
+    >>> # Assuming 'df' is your DataFrame
+    >>> pca_variance(df)
+    """
+    # dtype check
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("df must be a pandas DataFrame")
+
+    # Define scalers
+    scalers = [StandardScaler(), PowerTransformer()]
+
+    # Iterate over scalers
+    for scaler in scalers:
+        # Apply scaler and create a DataFrame with scaled values
+        scaled_df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
+    # Perform PCA on the scaled data
+    pca = PCA().fit(scaled_df)
+
+    # Plot the explained variance and cumulative explained variance
+    plt.vlines(20, 0, 1, ls="--", color="black", label=r"EV top 20 components")
+    plt.plot(np.concatenate([[0.0], pca.explained_variance_ratio_.cumsum()]), label="Cumsum EV")
+    plt.legend(loc="lower right")
+
+    # Customize plot
+    plt.xticks(range(0, 400, 100))
+    plt.xlim(-20, df.shape[1])
+    plt.xlabel("Components")
+    plt.ylabel("Explained variance")
+
+    pass
---
+++ brain_age_predictor/regression_pipelines.py
@@ -0,0 +1,336 @@
+"""
+Regression Pipelines Module
+
+This module provides functions for building and evaluating regression pipelines, specifically
+focused on linear regression with L1 regularization (:py:obj:`Lasso <sklearn.linear_model.Lasso>`)
+and neural network regression using Keras models.
+"""
+
+from itertools import product
+from typing import Iterable, Optional
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import PowerTransformer
+from sklearn.decomposition import PCA
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.utils import set_random_seed
+from scikeras.wrappers import KerasRegressor
+from numpy import typing as npt
+
+# Random Number Generator
+SEED = 20
+RNG = np.random.RandomState(SEED)
+
+# Set hidden layers and dropouts
+VALID_ENTRIES = [4, 8, 16, 32, 64]
+HIDDEN_LAYERS = list(product(VALID_ENTRIES, repeat=6))
+DROPOUTS = list(
+    RNG.choice([0.0, 0.1, 0.2, 0.3], p=[0.4, 0.2, 0.2, 0.2], size=(5_000, 6))
+)
+
+PARAMS = {
+    "mlp__nlayers": np.random.randint(1, 7, size=22),
+    "mlp__hiddens": HIDDEN_LAYERS,
+    "mlp__dropouts": DROPOUTS,
+    "mlp__optimizer__learning_rate": [0.0001, 0.001, 0.005],
+}
+
+
+def linear_regressor_pipeline(
+    df: npt.NDArray[np.number],
+    y: npt.NDArray[np.number],
+    n_components: int = 20,
+    thresh_overfit: float = 0.05,
+    scoring: str = "r2",
+    nsplits: int = 5,
+    seed: Optional[int] = None,
+    **kwargs,
+) -> pd.DataFrame:
+    """
+    Conducts a linear regression pipeline using L1 regularization
+    (:py:obj:`Lasso <sklearn.linear_model.Lasso>`).
+
+    Parameters
+    ----------
+    df : numpy.ndarray
+        Input dataset containing features as a 2D array of numerical values.
+
+    y : numpy.ndarray
+        Target variable as a 1D array of numerical values.
+
+    n_components : int (default=20)
+        Number of principal components to retain after dimensionality reduction.
+
+    thresh_overfit : float (default=0.05)
+        Threshold for identifying overfitting, defined as the difference between
+        mean training and test scores.
+
+    scoring : str (default="r2")
+        Scoring metric used for the cross-validated grid search. Default is R-squared.
+
+    nsplits : int (default=5)
+        Number of splits in the cross-validation strategy.
+
+    seed : int (default=None)
+        Seed for random state to ensure reproducibility.
+
+    **kwargs : Additional keyword arguments
+        Additional parameters to be passed to the
+        :py:obj:`GridSearchCV <sklearn.model_selection.GridSearchCV>` function.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing cross-validation results for different hyperparameter
+        settings. Only models without significant overfitting are included,
+        sorted by mean test score in descending order.
+
+    Raises
+    ------
+    ValueError
+        If ``n_components`` is less than 1.
+    ValueError
+        If ``thresh_overfit`` is negative.
+
+    Note
+    ----
+    This function implements a linear regression pipeline with L1 regularization
+    (:py:obj:`Lasso <sklearn.linear_model.Lasso>`).
+    It scales the input data, performs principal component analysis
+    (:py:obj:`PCA <sklearn.decomposition.PCA>`) for dimensionality
+    reduction, and uses (:py:obj:`Lasso <sklearn.linear_model.Lasso>`) regression as
+    the final model. The pipeline is optimized using grid search
+    with cross-validation to find the best hyperparameters.
+
+    The results DataFrame includes information about the hyperparameter settings,
+    training and test scores, and identifies models without significant overfitting.
+
+    Examples
+    --------
+
+    >>> df = ...
+    >>> y = ...
+    >>> results = linear_regressor_pipeline(df, y, n_components=15, nsplits=10, seed=42)
+    >>> print(results)
+    """
+    # set a random state so the results are reproducible
+    rng = np.random.RandomState(seed)
+
+    # Input check
+    if n_components < 1:
+        raise ValueError("n_compontents must be higher than 1.")
+    if thresh_overfit < 0:
+        raise ValueError("thresh_overfit must be positive.")
+
+    linear_regressor = Pipeline(
+        [
+            ("scaler", PowerTransformer()),
+            (
+                "principal_components",
+                PCA(
+                    n_components=n_components,
+                    svd_solver="full",
+                    random_state=rng,
+                ),
+            ),
+            ("model", Lasso(random_state=rng)),
+        ]
+    )
+
+    # L1 regularization
+    param_distr = {"model__alpha": np.arange(1, 10)}
+
+    search = GridSearchCV(
+        linear_regressor,
+        param_distr,
+        return_train_score=True,
+        scoring=scoring,
+        cv=KFold(n_splits=nsplits, shuffle=True, random_state=rng),
+        **kwargs,
+    )
+
+    search.fit(df, y)
+    print("Done!")
+
+    results_regressor = pd.DataFrame(search.cv_results_)
+    no_overfit = (
+        results_regressor["mean_train_score"] - results_regressor["mean_test_score"]
+        < thresh_overfit
+    )
+
+    return results_regressor[no_overfit].sort_values("mean_test_score", ascending=False)
+
+
+def n_layers_feed_forward(
+    nlayers: int,
+    hiddens: Iterable,
+    dropouts: Iterable,
+    meta: dict,  # Output KerasWrapper
+):
+    """
+    Create a feed-forward neural network with a specified number of layers, hidden units,
+    and dropout rates.
+
+    Parameters
+    ----------
+    nlayers : int
+        Number of layers in the neural network.
+
+    hiddens : Iterable
+        Iterable containing the number of hidden units for each layer.
+
+    dropouts : Iterable
+        Iterable containing the dropout rates for each layer.
+
+    meta : dict
+        Metadata dictionary containing information about the input shape for the neural network.
+
+    Returns
+    -------
+    Sequential
+        Keras Sequential model representing the specified feed-forward neural network.
+
+    Raises
+    ------
+    ValueError
+        If ``nlayers`` is less than 1.
+
+    Examples
+    --------
+
+    >>> nlayers = 3
+    >>> hiddens = [64, 128, 32]
+    >>> dropouts = [0.2, 0.4, 0.1]
+    >>> meta = {"X_shape_": (100,)}
+    >>> model = n_layers_feed_forward(nlayers, hiddens, dropouts, meta)
+    """
+    # set seed to have reproducible results
+    set_random_seed(SEED)
+    if nlayers < 1:
+        raise ValueError("nlayers must be higher than 1")
+    clf = Sequential()
+    X_shape_ = (meta["X_shape_"][1],)
+
+    clf.add(Dense(hiddens[0], activation="relu", input_shape=X_shape_))
+    if dropouts[0] > 0:
+        clf.add(Dropout(dropouts[0]))
+    for i in range(1, nlayers):
+        clf.add(Dense(hiddens[i], activation="relu"))
+        if dropouts[i] > 0:
+            clf.add(Dropout(dropouts[i]))
+    clf.add(Dense(1))
+    return clf
+
+
+def neural_network_pipeline(
+    mlp: KerasRegressor,  # Model
+    df: npt.NDArray[np.number],
+    y: npt.NDArray[np.number],
+    n_components: int = 20,
+    niter: int = 100,
+    thresh_overfit: float = 0.05,
+    scoring: str = "r2",
+    nsplits: int = 5,
+    seed: Optional[int] = None,
+    **kwargs,
+):
+    """
+    Conducts a pipeline for neural network regression using a specified Keras model.
+
+    Parameters
+    ----------
+    mlp : KerasRegressor
+        KerasRegressor model to be used in the pipeline.
+
+    df : numpy.ndarray
+        Input features as a 2D array of numerical values.
+
+    y : numpy.ndarray
+        Target variable as a 1D array of numerical values.
+
+    n_components : int (default=20)
+        Number of principal components to retain after dimensionality reduction.
+
+    niter : int (default=100)
+        Number of iterations for randomized search during hyperparameter tuning.
+
+    thresh_overfit : float (default=0.05)
+        Threshold for identifying overfitting, defined as the difference between
+        mean training and test scores.
+
+    scoring : str (default="r2")
+        Scoring metric used for the cross-validated randomized search. Default is R-squared.
+
+    nsplits : int (default=5)
+        Number of splits in the cross-validation strategy.
+
+    seed : int (default=None)
+        Seed for random state to ensure reproducibility.
+
+    **kwargs : Additional keyword arguments
+        Additional parameters to be passed to the
+        :py:obj:`RandomizedSearchCV <sklearn.model_selection.RandomizedSearchCV>`
+        function.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing cross-validation results for different hyperparameter
+        settings. Only models without significant overfitting are included,
+        sorted by mean test score in descending order.
+
+    Examples
+    --------
+    >>> from sklearn.neural_network import MLPRegressor
+    >>> import hp_search
+    >>> # Assuming 'df' and 'y' are your dataset and target variable
+    >>> mlp_model = MLPRegressor()
+    >>> results = neural_network_pipeline(mlp_model, df, y, n_components=15, nsplits=10, seed=42)
+    >>> print(results)
+    """
+
+    rng = np.random.RandomState(seed)
+    model = Pipeline(
+        [
+            ("scaler", PowerTransformer()),
+            (
+                "principal_components",
+                PCA(
+                    n_components=n_components,
+                    svd_solver="full",
+                    random_state=rng,
+                ),
+            ),
+            ("mlp", mlp),
+        ]
+    )
+
+    random_search = RandomizedSearchCV(
+        model,
+        PARAMS,
+        refit=False,
+        cv=KFold(nsplits, shuffle=True, random_state=rng),
+        return_train_score=True,
+        scoring=scoring,
+        n_iter=niter,
+        random_state=rng,
+        **kwargs,
+    )
+
+    print("Starting grid search neural_network...")
+    random_search.fit(df, y)
+    print("Done!")
+
+    df_results = pd.DataFrame(random_search.cv_results_)
+    no_overfit = (
+        df_results["mean_train_score"] - df_results["mean_test_score"] < thresh_overfit
+    )
+
+    return df_results[no_overfit].sort_values("rank_test_score")
