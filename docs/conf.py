5cb5b33e22038e68598dad838cbb3cb15753564d
Marco D'Arrigo <m.darrigo@studenti.unipi.it>
2024-01-22 01:55:31 +0100

    dddee

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,8 @@ extensions = [
     'sphinx_rtd_theme'
 ]
 
+# Mock the import of TensorFlow during documentation build
+autodoc_mock_imports = ["tensorflow"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
