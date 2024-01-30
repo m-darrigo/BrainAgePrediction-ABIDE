8534299de868840eada4b4d8596afc12781b87b8
Marco D'Arrigo <m.darrigo@studenti.unipi.it>
2024-01-30 23:08:39 +0100

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@
 import unittest
 import numpy as np
 
-import library
+import preprocessing_lib as lib
 
 
 class TestPreprocessing(unittest.TestCase):
@@ -12,12 +12,22 @@ class TestPreprocessing(unittest.TestCase):
     def test_alldistr_img(self):
         # This is a test to see if the function runs without errors.
         # The function is so simple that it does not need deep testing
-        library.all_distr_img(self.df)
+        lib.alldistr_img(self.df)
+
+    def test_corr_distr(self):
+        # This is a test to see if the function runs without errors.
+        # The function is so simple that it does not need deep testing
+        lib.corr_distr(self.df)
+
+    def test_scaled_distributions(self):
+        # This is a test to see if the function runs without errors.
+        # The function is so simple that it does not need deep testing
+        lib.scaled_distributions(self.df)
 
     # test if the function returns a ValueError when a wrong input is given.
     def test_alldistr_img_wrong_dtype(self):
         with self.assertRaises(ValueError):
-            library.all_distr_img(5)
+            lib.alldistr_img(5)
 
     # This type of testing can be utilized for all plotting functions
 
